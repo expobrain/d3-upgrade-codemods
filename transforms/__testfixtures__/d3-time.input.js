021b231d28317bf3294e2c1d98c620d1fe27504d
Daniele Esposti <daniele.esposti@corp.badoo.com>
2016-11-06 11:06:12 +0000

    Transforms d3.time

---
+++ transforms/__testfixtures__/d3-time.input.js
@@ -0,0 +1,6 @@
+// Straight conversion
+d3.time.scale
+d3.time.scale.utc
+
+// scale() method
+d3.svg.axis().scale(x)
---
+++ transforms/__testfixtures__/d3-time.output.js
@@ -0,0 +1,6 @@
+// Straight conversion
+d3.scaleTime
+d3.scaleUtc
+
+// scale() method
+d3.svg.axis().scale(x)
