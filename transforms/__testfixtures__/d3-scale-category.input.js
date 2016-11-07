febc43dd93f08d182c3f20a01626ded031fd88fe
Daniele Esposti <daniele.esposti@corp.badoo.com>
2016-11-07 22:19:13 +0000

    Transforms d3.scale.category

---
+++ transforms/__testfixtures__/d3-scale-category.input.js
@@ -0,0 +1,8 @@
+// straight transofrmation
+d3.scale.category10()
+d3.scale.category20()
+d3.scale.category20b()
+d3.scale.category20c()
+
+// Remove range()
+d3.scale.category10().range()
---
+++ transforms/__testfixtures__/d3-scale-category.output.js
@@ -0,0 +1,8 @@
+// straight transofrmation
+d3.schemeCategory10
+d3.schemeCategory20
+d3.schemeCategory20b
+d3.schemeCategory20c
+
+// Remove range()
+d3.schemeCategory10
