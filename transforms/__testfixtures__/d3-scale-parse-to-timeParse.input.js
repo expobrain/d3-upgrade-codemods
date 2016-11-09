f849bab2ab7f7c0c95a1ce61838947b181c0a3c8
Daniele Esposti <daniele.esposti@corp.badoo.com>
2016-11-09 19:23:13 +0000

    Transforms d3.scale interpolate() into curve()

---
+++ transforms/__testfixtures__/d3-scale-parse-to-timeParse.input.js
@@ -0,0 +1,1 @@
+d3.timeFormat("fmt").parse
---
+++ transforms/d3-scale-parse-to-timeParse.js
@@ -0,0 +1,30 @@
+import { isCallExpression } from './utils'
+
+/**
+ * https://github.com/d3/d3/blob/master/CHANGES.md#scales-d3-scale
+ *
+ * d3.timeFormat(<fmt>).parse ↦ d3.timeParse(<fmt>)
+ */
+export default function transformer(file, api) {
+  const j = api.jscodeshift
+  const root = j(file.source)
+
+  root
+    .find(j.MemberExpression, {
+      property: {
+        type: 'Identifier',
+        name: 'parse'
+      }
+    })
+    .filter((path) => isCallExpression(path.node.object, 'd3.timeFormat'))
+    .replaceWith((path) => {
+      const callee = j.memberExpression(path.node.object.callee.object, j.identifier('timeParse'))
+      const callExpression = j.callExpression(callee, path.node.object.arguments)
+
+      callExpression.callee.property = j.identifier('timeParse')
+
+      return callExpression
+    })
+
+  return root.toSource()
+}
