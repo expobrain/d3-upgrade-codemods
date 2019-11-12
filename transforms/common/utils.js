6ecca2a8f10bae0d66e404217fbf4360f80932df
Daniele Esposti <daniele.esposti@gmail.com>
2019-11-12 22:17:46 +0000

    Format all the code

--- transforms/common/utils.js
+++ transforms/common/utils.js
@@ -1,46 +1,44 @@
 export const isMemberExpression = (node, literal) => {
-    if (node.type !== 'MemberExpression') {
-      return false
-    }
+  if (node.type !== "MemberExpression") {
+    return false;
+  }
 
-    const tokens = literal.split('.')
-    const prefix = tokens.slice(0, tokens.length - 1)
-    const name = tokens[tokens.length - 1]
-    let object
+  const tokens = literal.split(".");
+  const prefix = tokens.slice(0, tokens.length - 1);
+  const name = tokens[tokens.length - 1];
+  let object;
 
-    if (prefix.length > 1) {
-      object = isMemberExpression(node.object, prefix.join('.'))
-    } else {
-      object = isIdentifier(node.object, prefix[0])
-    }
+  if (prefix.length > 1) {
+    object = isMemberExpression(node.object, prefix.join("."));
+  } else {
+    object = isIdentifier(node.object, prefix[0]);
+  }
 
-    return object && isIdentifier(node.property, name)
-}
+  return object && isIdentifier(node.property, name);
+};
 
 export const isIdentifier = (node, name) => {
-  return node.type === 'Identifier' && node.name === name
-}
+  return node.type === "Identifier" && node.name === name;
+};
 
-
-export const buildMemberExpressionFromLiteral = (literal) => (j) => {
-  const tokens = literal.split('.')
+export const buildMemberExpressionFromLiteral = literal => j => {
+  const tokens = literal.split(".");
 
   if (tokens.length < 2) {
-    return j.identifier(tokens[0])
+    return j.identifier(tokens[0]);
   }
 
-  const literalPrefix = tokens.slice(0, tokens.length - 1).join('.')
-  const object = buildMemberExpressionFromLiteral(literalPrefix)(j)
-  const property = j.identifier(tokens[tokens.length - 1])
+  const literalPrefix = tokens.slice(0, tokens.length - 1).join(".");
+  const object = buildMemberExpressionFromLiteral(literalPrefix)(j);
+  const property = j.identifier(tokens[tokens.length - 1]);
 
-  return j.memberExpression(object, property)
-}
-
+  return j.memberExpression(object, property);
+};
 
 export const isCallExpression = (node, literal) => {
-  if (node.type !== 'CallExpression') {
-    return false
+  if (node.type !== "CallExpression") {
+    return false;
   }
 
-  return isMemberExpression(node.callee, literal)
-}
+  return isMemberExpression(node.callee, literal);
+};
